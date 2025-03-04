603f0738059587b74cd5c2086f13e0ee602c7895
techw4all <team.techworld4all@gmail.com>
2025-03-04 17:30:52 -0300

    fix: instagram account

--- resources/static/contact.ts
+++ resources/static/contact.ts
@@ -2,5 +2,6 @@ export type ContactWay = string | null;
 
 
 export const linkedin: ContactWay = null;
+export const instagram: ContactWay = null;
 export const emailAddress: ContactWay = 'contato.br@w4io.com';
 export const phoneNumber: (() => ContactWay) = () => process.env.NEXT_PUBLIC_CONTACT_PHONE || null;
